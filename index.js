53941761af8155059833de3832077dbb04efbf1a
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2023-05-07 15:14:55 -0300

    Projeto parcialmente ok

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const sideMenu = document.querySelector('aside')
 const menuBtn = document.querySelector('#menu-btn')
 const closeBtn = document.querySelector('#close-btn')
+const themeToggler = document.querySelector('.theme-toggle')
 
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block'
-    
+// show sidebar
+menuBtn.addEventListener('click', () => sideMenu.style.display = 'block')
+// close sidebar
+closeBtn.addEventListener('click', () => sideMenu.style.display = 'none')
+// close theme
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables')
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
 })
 
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none'
-
+Orders.forEach(order => {
+    const tr = document.createElement('tr')
+    const trContent = `
+                <td>${order.productName}</td>
+                <td>${order.productNumber}</td>
+                <td>${order.paymentStatus}</td>
+                <td class="${order.shipping === 'Em trânsito' ? 'danger' : order.shipping === 'Entregue'? 'success' : 'primary'}">${order.shipping}</td>
+                <td class="primary"><a href="#">Detalhes</a></td>
+    `
+    tr.innerHTML = trContent
+    document.querySelector('table tbody').appendChild(tr)
 })
+
+
---
+++ orders.js
@@ -0,0 +1,92 @@
+const Orders = [
+    {
+        productName: 'Shampoo Siage Eudora',
+        productNumber: '3656',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Condicionador Dove',
+        productNumber: '7869',
+        paymentStatus: 'Pendente',
+        shipping: 'Em trânsito'
+      },
+      {
+        productName: 'Máscara facial Nivea',
+        productNumber: '2435',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Batom MAC',
+        productNumber: '1234',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Esmalte Risqué',
+        productNumber: '6743',
+        paymentStatus: 'Pendente',
+        shipping: 'Em trânsito'
+      },
+      {
+        productName: 'Perfume Chanel',
+        productNumber: '5678',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Hidratante Corporal Natura',
+        productNumber: '9876',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Protetor solar La Roche-Posay',
+        productNumber: '4567',
+        paymentStatus: 'Pendente',
+        shipping: 'Em trânsito'
+      },
+      {
+        productName: 'Balm labial Bepantol',
+        productNumber: '3456',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Gel de limpeza facial Neutrogena',
+        productNumber: '7890',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Perfume masculino Polo',
+        productNumber: '2345',
+        paymentStatus: 'Pendente',
+        shipping: 'Em trânsito'
+      },
+      {
+        productName: 'Sérum capilar L\'Oréal',
+        productNumber: '8901',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Esmalte Revlon',
+        productNumber: '5678',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      },
+      {
+        productName: 'Shampoo Head & Shoulders',
+        productNumber: '1234',
+        paymentStatus: 'Pendente',
+        shipping: 'Em trânsito'
+      },
+      {
+        productName: 'Batom Maybelline',
+        productNumber: '4567',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+      }
+]
